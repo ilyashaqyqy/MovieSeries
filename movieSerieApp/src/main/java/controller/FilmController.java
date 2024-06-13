1621b9a885f23a341ca8bb2c21d524c63058a44e
ilyashq <ilyasenaa24@gmail.com>
2024-06-13 11:38:35 +0100

    Ajout du contrôleur FilmController

---
+++ movieSerieApp/src/main/java/controller/FilmController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import model.Film;
+import service.FilmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/films")
+public class FilmController {
+
+    @Autowired
+    private FilmService filmService;
+
+    @GetMapping
+    public List<Film> getAllFilms() {
+        return filmService.getAllFilms();
+    }
+
+    @PostMapping
+    public Film saveFilm(@RequestBody Film film) {
+        return filmService.saveFilm(film);
+    }
+
+
+}
